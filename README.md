dfd40a56fdcbeb132cf44a28b800cb88ed65d4a7
Adam Smith <liversmiles@gmail.com>
2024-12-03 11:15:21 -0500

    Added files.
    These added folders and files for standard setup.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Prime_Number_V2
-This is version 2 of prime numbers. The genesis block starts a 0 to 2^32. The array's are bit array's and their size is 2^32.
+# Prime Number V2
+This is version 2 of prime numbers.
+
+The genesis block starts a 0 to 2^32. 
+
+The arrays consists of bits and its size is 2^32.
+
+Once a block is copleted. it is then compressed with a base 174.
---
+++ src/main.py
@@ -0,0 +1,27 @@
+"""
+Prime Number V2 Program
+Copyright (c) 2024 Adam Smith
+
+Open Source Software under the Prime Number V2 Program License (Modified MIT)
+For commercial use, contact liversmiles@gmail.com.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, subject to the conditions specified in the license.
+"""
+
+
+# Standard Library Imports
+
+# Third-Party Imports
+
+# Local Imports
+
+def main() -> None:
+    # This is the primary function to start your program.
+
+    return
+
+
+if __name__ == '__main__':
+    main()
